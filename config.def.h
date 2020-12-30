57c9b1c3c5c7ad946a9198693e508cbc90a877ae
kocotian <kocotian@kocotian.pl>
2020-12-30 11:58:24 +0100

    colors, own vimkeys patch for better steering

--- config.def.h
+++ config.def.h
@@ -11,10 +11,10 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*                fg         bg       */
+	[SchemeNorm] = { "#aaaaaa", "#222222" },
+	[SchemeSel] =  { "#ffffff", "#222222" },
+	[SchemeOut] =  { "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
--- dmenu.c
+++ dmenu.c
@@ -439,14 +439,13 @@ keypress(XKeyEvent *ev)
 		case XK_g: ksym = XK_Escape;    break;
 		case XK_h: ksym = XK_BackSpace; break;
 		case XK_i: ksym = XK_Tab;       break;
-		case XK_j: /* fallthrough */
-		case XK_J: /* fallthrough */
-		case XK_m: /* fallthrough */
 		case XK_M: ksym = XK_Return; ev->state &= ~ControlMask; break;
+		case XK_j: /* fallthrough */
 		case XK_n: ksym = XK_Down;      break;
+		case XK_k: /* fallthrough */
 		case XK_p: ksym = XK_Up;        break;
 
-		case XK_k: /* delete right */
+		case XK_r: /* delete right */
 			text[cursor] = '\0';
 			match();
 			break;
