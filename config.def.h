8eac900386315e83003af559e9f0db64f499fd86
kocotian <kocotian@kocotian.pl>
2021-03-02 20:14:34 +0100

    colors

--- config.def.h
+++ config.def.h
@@ -6,15 +6,18 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=8",
+	"monospace:size=8:antialias=true:autohint=true",
 	"JoyPixels:pixelsize=10:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                fg         bg       */
-	[SchemeNorm]   = { "#aaaaaa", "#000000" },
-	[SchemeSel]    = { "#ffffff", "#222222" },
-	[SchemePrpt]   = { "#ffffff", "#222222" },
+	[SchemeNorm]   = { "#ebdbb2", "#000000" },
+	[SchemeSel]    = { "#fffff7", "#222222" },
+	[SchemePrpt]   = { "#fffff7", "#222222" },
+	/* [SchemeNorm]   = { "#aaaaaa", "#000000" }, */
+	/* [SchemeSel]    = { "#ffffff", "#222222" }, */
+	/* [SchemePrpt]   = { "#ffffff", "#222222" }, */
 	[SchemeLnNorm] = { "#222222", "#222222" },
 	[SchemeLnSel]  = { "#98971a", "#98971a" },
 	[SchemeOut]    = { "#000000", "#00ffff" },
